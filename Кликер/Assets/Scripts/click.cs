019a2b074662c462b3c3929b2483ffc7f45ba989
KoT91544 <67955094+KoT91544@users.noreply.github.com>
2020-07-07 15:31:38 +0300

    project Unity

---
+++ Кликер/Assets/Scripts/click.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System; 
+
+public class click : MonoBehaviour
+{
+    float add_to_click = 1;
+    public float maney = 0;
+    public Text numbermoney;
+    public float time;
+    float deltime;
+    float profit_auto = 1;
+    bool start_auto = false;
+    float price_auto = 10;
+    private int count = 0;
+    private GameObject destoring;
+    public GameObject[] Buildings;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        deltime = time;
+        destoring = Instantiate(Buildings[count]);
+        count++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+        numbermoney.text = ("Money:" + maney);
+        if (start_auto == true)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                maney += profit_auto;
+                time = deltime;
+
+            }
+        }
+    }
+    public void Click()
+    {
+        maney += add_to_click;
+
+    }
+    public void Autoclicker()
+    {
+        if (start_auto == false && maney >= price_auto)
+        {
+            start_auto = true;
+            maney -= price_auto;
+            price_auto *= 2f;
+
+
+        }
+        if (start_auto == true && maney >= price_auto)
+        {
+            profit_auto += 1;
+            maney -= price_auto;
+            price_auto *= 2f ;
+            Math.Round(price_auto, 1);
+            
+
+        }
+    }
+    public void upgrade_click()
+    {
+        if (maney >= 100)
+        {
+            add_to_click += 2;
+            maney -= 100; 
+        }
+
+
+    }
+    public void Upgarde_building()
+    {
+        if(maney >= 1000)
+        {
+            Destroy(destoring);
+            destoring = Instantiate(Buildings[count]);
+            count++;
+            Debug.Log("Объект заспавнен " + count);
+            maney -= 1000;
+        }
+        
+    }
+
+
+
+}
---
+++ Кликер/Assets/Scripts/menutomenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menutomenu : MonoBehaviour
+ {
+
+    public GameObject  shop_click ;
+    public GameObject  shop_auto ;
+    public GameObject shop_end_auto;
+    public GameObject shop_end_click;
+    public GameObject clicker;
+    public GameObject upgrades_auto;
+    public GameObject upgrades_click;
+    public GameObject shop_building;
+    public GameObject shop_end_building;
+    public GameObject upgrade_building;
+
+    public void shop_click1()
+    {
+        clicker.gameObject.active = false;
+        upgrades_auto.gameObject.active = false;
+        upgrades_click.gameObject.active = true;
+        shop_end_click.gameObject.active = true;
+        shop_auto.gameObject.active = true;
+        shop_end_auto.gameObject.active = false;
+        shop_click.gameObject.active = false;
+        upgrade_building.gameObject.active = false;
+        shop_end_building.gameObject.active = false;
+        shop_building.gameObject.active = true;
+    }
+    public void shop_auto1()
+    {
+        clicker.gameObject.active = false;
+        upgrades_auto.gameObject.active = true;
+        upgrades_click.gameObject.active = false;
+        shop_end_click.gameObject.active = false;
+        shop_end_auto.gameObject.active = true;
+        shop_click.gameObject.active = true;
+        shop_auto.gameObject.active = false;
+        upgrade_building.gameObject.active = false;
+        shop_end_building.gameObject.active = false;
+        shop_building.gameObject.active = true;
+    }
+    public void shop_end()
+    {
+        clicker.gameObject.active = true;
+        upgrades_click.gameObject.active = false;
+        upgrades_auto.gameObject.active = false;
+        shop_click.gameObject.active = true;
+        shop_auto.gameObject.active = true;
+        shop_end_auto.gameObject.active = false;
+        shop_end_click.gameObject.active = false;
+        upgrade_building.gameObject.active = false;
+        shop_end_building.gameObject.active = false;
+        shop_building.gameObject.active = true;
+    }
+    public void shop_building1 ()
+    {
+        clicker.gameObject.active = false;
+        upgrades_auto.gameObject.active = false;
+        upgrades_click.gameObject.active = false;
+        shop_end_click.gameObject.active = false;
+        shop_auto.gameObject.active = true;
+        shop_end_auto.gameObject.active = false;
+        shop_click.gameObject.active = true;
+        upgrade_building.gameObject.active = true;
+        shop_end_building.gameObject.active = true;
+        shop_building.gameObject.active = false;
+
+    }
+}
+
